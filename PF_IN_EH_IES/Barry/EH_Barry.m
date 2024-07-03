6708b478058acc65e3b7c92d955b4b734b4f82e2
Eric6669 <77628887+Eric6669@users.noreply.github.com>
2024-07-03 23:26:49 +0800

    Create EH_Barry.m

---
+++ PF_IN_EH_IES/Barry/EH_Barry.m
@@ -0,0 +1,16 @@
+% /*
+%  * @Descripttion: 
+%  * @version: 
+%  * @Author: Ke Wang
+%  * @Date: 2024-07-03 15:05:34
+%  * @LastEditors: Ke Wang
+%  * @LastEditTime: 2024-07-03 15:09:39
+%  */
+
+
+
+
+
+
+
+
