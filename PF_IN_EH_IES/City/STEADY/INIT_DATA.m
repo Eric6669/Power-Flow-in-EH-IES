e5adb74b3aff1dd16c9fa73d0206f40727aecb08
Eric6669 <77628887+Eric6669@users.noreply.github.com>
2024-07-08 15:23:22 +0800

    Update INIT_DATA.m

--- PF_IN_EH_IES/City/STEADY/INIT_DATA.m
+++ PF_IN_EH_IES/City/STEADY/INIT_DATA.m
@@ -6,7 +6,7 @@
 %  * @LastEditors: Ke Wang
 %  * @LastEditTime: 2024-07-06 20:08:29
 %  */
-m = 50*ones(npipes,1);
+m = ones(npipes,1);
 % node temperature
 Ta = -10*ones(nnodes,1);
 % assume load supply temperature 
@@ -15,6 +15,6 @@ Ts = 80*ones(nnodes,1)-Ta;
 To = 40*ones(nnodes,1)-Ta;
 Tr = To; 
 
-U = 1.05*ones(n,1);
-cita = zeros(n,1);
+U = 1.05*ones(1,n);
+cita = zeros(1,n);
 cita = (deg2rad(cita));  % degree2rad
